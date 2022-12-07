35b3423f2fef70a4a3b9a4fd42f2038c45e56b6f
rajarshimaitra <rajarshi149@gmail.com>
2022-12-07 17:31:51 +0530

    Add a electrum syncing example
    The electrum sync is done in two steps.
     - `wallet_txid_scan` returns a candidate `KeychainScan` with TxGraph
     data.
     - The candidate is compared with tracker's `SparseChain`, and list of
     new tx data to fetch is derived from the `ChangeSet`.
     - New transactions are fetched in batch and added into the `KeychainScan`
     update.
     - Apply the final `KeychainScan` on tracker and DB.

---
+++ bdk_electrum_example/src/main.rs
@@ -0,0 +1,151 @@
+mod electrum;
+
+use electrum::ElectrumClient;
+
+use bdk_cli::{
+    anyhow::{self, Context},
+    handle_commands, Commands,
+};
+use log::debug;
+
+use electrum_client::ElectrumApi;
+
+fn main() -> anyhow::Result<()> {
+    let (args, keymap, mut tracker, mut db) = bdk_cli::init()?;
+
+    let client = ElectrumClient::new("ssl://electrum.blockstream.info:60002")?;
+
+    match args.command {
+        Commands::Scan { stop_gap } => {
+            let last_known_height = tracker
+                .chain()
+                .checkpoints()
+                .iter()
+                .last()
+                .map(|(&ht, _)| ht);
+
+            let scripts = tracker.txout_index.iter_all_script_pubkeys_by_keychain();
+
+            let mut keychain_scan =
+                client.wallet_txid_scan(scripts, Some(stop_gap), last_known_height)?;
+
+            // Update `keychain_scan` with missing transactions
+            let txid_changeset = tracker
+                .chain_graph()
+                .determine_changeset(&keychain_scan.update)?
+                .chain
+                .txids
+                .into_iter()
+                .filter(|item| !tracker.graph().contains_txid(item.0))
+                .collect::<Vec<_>>();
+
+            let new_transactions =
+                client.batch_transaction_get(txid_changeset.iter().map(|(txid, _)| txid))?;
+
+            for (tx, index) in new_transactions.into_iter().zip(
+                txid_changeset
+                    .iter()
+                    .map(|(_, index)| index.expect("expect")),
+            ) {
+                keychain_scan.update.insert_tx(tx, index)?;
+            }
+
+            // Apply the full scan update
+            let changeset = tracker.determine_changeset(&keychain_scan)?;
+            db.append_changeset(&changeset)?;
+            tracker.apply_changeset(changeset);
+            debug!("sync completed!!")
+        }
+        Commands::Sync {
+            mut unused,
+            mut unspent,
+            all,
+        } => {
+            let txout_index = &tracker.txout_index;
+            if !(all || unused || unspent) {
+                unused = true;
+                unspent = true;
+            } else if all {
+                unused = false;
+                unspent = false
+            }
+            let mut spks: Box<dyn Iterator<Item = bdk_core::bitcoin::Script>> =
+                Box::new(core::iter::empty());
+            if unused {
+                spks = Box::new(spks.chain(txout_index.iter_unused().map(|(index, script)| {
+                    eprintln!("Checking if address at {:?} has been used", index);
+                    script.clone()
+                })));
+            }
+
+            if all {
+                spks = Box::new(spks.chain(txout_index.script_pubkeys().iter().map(
+                    |(index, script)| {
+                        eprintln!("scanning {:?}", index);
+                        script.clone()
+                    },
+                )));
+            }
+
+            if unspent {
+                spks = Box::new(spks.chain(tracker.utxos().map(|(_index, ftxout)| {
+                    eprintln!("checking if {} has been spent", ftxout.outpoint);
+                    ftxout.txout.script_pubkey
+                })));
+            }
+
+            let last_known_height = tracker
+                .chain()
+                .checkpoints()
+                .iter()
+                .last()
+                .map(|(&ht, _)| ht);
+
+            let mut chaingraph_update = client
+                .spk_txid_scan(spks, last_known_height)
+                .context("scanning the blockchain")?;
+
+            // Update `chaingraph_update` with missing transaction data.
+            let txid_changeset = tracker
+                .chain_graph()
+                .determine_changeset(&chaingraph_update)?
+                .chain
+                .txids
+                .into_iter()
+                .filter(|item| !tracker.graph().contains_txid(item.0))
+                .collect::<Vec<_>>();
+
+            // Fetch and add these new transactions into update candidate
+            let new_transactions =
+                client.batch_transaction_get(txid_changeset.iter().map(|(txid, _)| txid))?;
+
+            for (tx, index) in new_transactions.into_iter().zip(
+                txid_changeset
+                    .iter()
+                    .map(|(_, index)| index.expect("expect")),
+            ) {
+                chaingraph_update.insert_tx(tx, index)?;
+            }
+
+            // Apply `chaingraph_update`.
+            let changeset = tracker
+                .chain_graph()
+                .determine_changeset(&chaingraph_update)?
+                .into();
+            db.append_changeset(&changeset)?;
+            tracker.apply_changeset(changeset);
+
+            debug!("sync completed!!");
+        }
+        // For everything else run handler
+        _ => handle_commands(
+            args.command,
+            client,
+            &mut tracker,
+            &mut db,
+            args.network,
+            &keymap,
+        )?,
+    }
+    Ok(())
+}
